f9d1c63968ac0fa7ebf7b9ba4627429f7549aa37
xiaoyun <xiaoyun@MacBook-Air-2.local>
2017-05-26 18:10:58 -0500

    Add Strategy

---
+++ Strategy/src/Func1DifferInSomeSubClass.java
@@ -0,0 +1,6 @@
+/**
+ * Created by xiaoyun on 5/26/17.
+ */
+public interface Func1DifferInSomeSubClass {
+    public void run();
+}
---
+++ Strategy/src/Func2Category2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by xiaoyun on 5/26/17.
+ */
+public class Func2Category2 implements Func2DifferInSomeSubClass{
+    public void run() {
+        System.out.println("func2DifferInSomeSubClass, Category 2");
+    }
+}
